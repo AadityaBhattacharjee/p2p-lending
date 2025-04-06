d1b7b5296b347d2eb677f915224d217d024c8c12
AadityaBhattacharjee <95535792+AadityaBhattacharjee@users.noreply.github.com>
2025-04-06 09:57:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -234,5 +234,14 @@ def logout():
     return redirect('/login')
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get("PORT", 5000))
+    import os
+
+    PORT = os.environ.get("PORT")
+    if PORT:
+        PORT = int(PORT)
+    else:
+        PORT = 5000
+
+    print(f"Starting on port {PORT}")
     app.run(host='0.0.0.0', port=PORT)
+
